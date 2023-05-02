64040fde4ddce851008f7a9eb6b6bf02f5ce7c29
LionelCoutreau <liangelus33+github@gmail.com>
2023-05-02 17:03:33 +0200

    Début de préparation de la page Index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,32 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { city, country, name, portrait, price, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        // bloc carte
         const article = document.createElement( 'article' );
+        // image
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        // titre
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const residence = document.createElement('div');
+        residence.className = 'residence';
+        residence.textContent = `${city}, ${country}`;
+        const tag = document.createElement('div');
+        tag.className = 'tagline';
+        tag.textContent = tagline;
+        const tarif = document.createElement('div');
+        tarif.className = 'price';
+        tarif.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(residence);
+        article.appendChild(tag);
+        article.appendChild(tarif);
         return (article);
     }
     return { name, picture, getUserCardDOM }
